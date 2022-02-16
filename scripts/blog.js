7af5d50e410f6bbb259912d47fb8934324028cfb
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2022-02-16 18:52:06 +0100

    Added file caching

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,3 +1,8 @@
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+  console.log("localstoreage cleared")
+  localStorage.clear()
+}
+
 // quasi main
 document.addEventListener("DOMContentLoaded", async (event) => {
   let params = new URLSearchParams(window.location.search)
@@ -19,12 +24,10 @@ document.addEventListener("DOMContentLoaded", async (event) => {
       hljs.highlightElement(el)
     })
 
-    updateSocialLinks(yaml)
-
+  updateSocialLinks(yaml)
 })
 
-
-function updateSocialLinks({title, markdown, desc}) {
+function updateSocialLinks({ title, markdown, desc }) {
   const doc = document
 
   const fb = doc.querySelector("#social-facebook")
@@ -33,13 +36,17 @@ function updateSocialLinks({title, markdown, desc}) {
   const copy = doc.querySelector("#social-copy")
 
   const link = encodeURI(`${location.origin}/blog.html?p=${markdown}`)
-  
+
   console.log(link)
   fb.setAttribute("href", "/")
 }
 
 async function getYamlEntry(post) {
-  let postYAML = await fetch("./blog-list.yaml").then((res) => res.text())
+  let postYAML = getWithExpiry("blog-list.yaml")
+  if (!postYAML) {
+    postYAML = await fetch("blog-list.yaml").then((res) => res.text())
+    setWithExpiry("blog-list.yaml", postYAML, YAML_TIMEOUT)
+  }
 
   let posts = jsyaml.load(postYAML)
 
@@ -54,19 +61,31 @@ async function getYamlEntry(post) {
 async function renderPost(yaml, post) {
   let canvas = document.querySelector("#out")
 
-  let md = await fetch("./posts/" + post + ".md").then((res) => {
-    if (res.status === 404) {
-      return undefined
-    }
-    return res.text()
-  })
+  let md = getWithExpiry(post)
   if (!md) {
-    window.location.href = "./404.html"
+    console.log("Fetching new post")
+    md = await fetch("./posts/" + post + ".md").then((res) => {
+      if (res.status === 404) {
+        return undefined
+      }
+      return res.text()
+    })
+
+    if (!md) {
+      window.location.href = "./404.html"
+    }
+
+    setWithExpiry(post, md, BLOG_TIMEOUT)
   }
+
   let title = yaml.title
   document.title = title
 
   let converter = new showdown.Converter()
   let html = converter.makeHtml(md)
   canvas.innerHTML = html
+}
+
+function hellower() {
+  console.log("hello world")
 }
---
+++ scripts/utils.js
@@ -0,0 +1,32 @@
+// two hours in millis
+const BLOG_TIMEOUT = 7200000
+
+// 30 minutes in millis
+const YAML_TIMEOUT = 1800000
+
+function setWithExpiry(key, value, timeout) {
+  const now = new Date().getTime()
+  const item = {
+    value: value,
+    expiry: now + timeout,
+  }
+  localStorage.setItem(key, JSON.stringify(item))
+  console.log(item)
+}
+
+function getWithExpiry(key) {
+  const itemjson = localStorage.getItem(key)
+
+  if (!itemjson) return null
+
+  const item = JSON.parse(itemjson)
+
+  const now = new Date().getTime()
+
+  if (now >= item.expiry) {
+    localStorage.removeItem(key)
+    return null
+  }
+
+  return item.value
+}
